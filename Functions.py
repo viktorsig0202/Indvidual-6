b0bab0f8fc525a7187e81213f5445290504c5fb2
Viktor Sigurðsson <viktorsig9@gmail.com>
2024-03-10 17:34:41 +0000

    added user class for the funcitons

--- Functions.py
+++ Functions.py
@@ -1,8 +1,15 @@
-def User_Sign_Up():
-    pass
-def Monthly_Calander():
-    pass
-def Reccomendations():
-    pass
-def Comment_On_Activity():
-    pass
+class User():
+    def __init__(self,username):
+        self.username = username
+
+
+
+
+    def User_Sign_Up():
+        pass
+    def Monthly_Calander():
+        pass
+    def Reccomendations():
+        pass
+    def Comment_On_Activity():
+        pass
