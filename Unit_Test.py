2bed4d6fca5f3c29508c264fa7fb98424deef942
Viktor Sigurðsson <viktorsig9@gmail.com>
2024-03-10 19:45:33 +0000

    minor changes to docstrings

--- Unit_Test.py
+++ Unit_Test.py
@@ -9,13 +9,14 @@ class TestUserStories(unittest.TestCase):
         Test case for the User_Sign_Up function.
         This test checks if the user sign up process was a success or a fail.
 
-        Input: - None
+        Args:
+        None
 
         Example:
         >>> assertTrue(result)
 
         Returns:
-        - Bool
+        Bool: True if the signup procces is successful and False if it fails
         """
         is_signed_in = Functions.User_Sign_Up()
         self.assertTrue(is_signed_in)
@@ -24,15 +25,16 @@ class TestUserStories(unittest.TestCase):
         """
         Test case for the Montly_Calander function.
         This test checks if the montly Calander is not None.
-        if it is None, the function failes.
+        if it is None, the function failed.
 
-        Input: - None
+        Args:
+        None
 
         Example:
         >>> assertIsNotNone(Montly_events)
 
         Returns:
-        - Monthly_Calander: List containing events for the current month
+        List: List containing events for the current month
         """
         monthly_events = Functions.Monthly_Calander()
         self.assertIsNotNone(monthly_events)
@@ -42,14 +44,15 @@ class TestUserStories(unittest.TestCase):
         Test case for Reccomendation funciton.
         This test checks if the Reccomendation align with the users preferences.
 
-        Input: - user_interests: A list containing the user_interests
+        Args:
+        user_interests(List): A list containing the user_interests
 
         Example:
         >>> Reccomendation(["hiking"]) = "hiking_activity"
         assertEqual("hiking","hiking activity")
 
         Returns:
-        - Activity: a string that the system reccomends to the user
+        String: a activity in the form of a string that the system reccomends to the user
         """
         interests = ["climbing"]
         result = Functions(interests)
@@ -60,14 +63,15 @@ class TestUserStories(unittest.TestCase):
         Test case for Comment_On_Activity funciton.
         this checks if the comment matched the comment on the activity
 
-        Input: - comment: string
+        Args:
+        comment(string): the comment the user wants to post on the activity
 
         Example:
         >>> comment = "string" -> Result = Comment_On_Activity(comment)
         assertEqual(comment,Result)
 
         Returns:
-        -comment_on_activity: a string comment on the on the activity
+        string: the comment in the form of a string which is posted on the activity
         """
         comment = "I Really Liked it!"
         comment_on_activity = Functions.Comment_On_Activity(comment)
