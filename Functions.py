5ed14f8a6f495e48f2a14f7972a0575adb7ec225
Viktor Sigurðsson <viktorsig9@gmail.com>
2024-03-10 17:37:38 +0000

    deleted class

--- Functions.py
+++ Functions.py
@@ -1,15 +1,9 @@
-class User():
-    def __init__(self,username):
-        self.username = username
 
-
-
-
-    def User_Sign_Up():
-        pass
-    def Monthly_Calander():
-        pass
-    def Reccomendations():
-        pass
-    def Comment_On_Activity():
-        pass
+def User_Sign_Up():
+    pass
+def Monthly_Calander():
+    pass
+def Reccomendations():
+    pass
+def Comment_On_Activity():
+    pass
