f35424769b2a1f2a96ea4c638033c8d87aeab447
Viktor Sigurðsson <viktorsig9@gmail.com>
2024-03-16 23:11:21 +0000

    added Monthly_Calander Function

--- Functions.py
+++ Functions.py
@@ -16,7 +16,33 @@ def User_Sign_Up(username="", password=""):
 
 
 def Monthly_Calander():
-    pass
+    """
+    Function to generate montly calender event
+
+    Returns:
+    List: List containing events for the current month
+    """
+    import datetime
+
+    schedule = {
+        "January": ["Skiing", "Snowmobiling"],
+        "February": ["Ice climbing", "Winter hiking"],
+        "March": ["Glacier hiking", "Snowshoeing"],
+        "April": ["Volcano trekking", "Cave exploration"],
+        "May": ["Kayaking", "Whale watching"],
+        "June": ["River rafting", "Camping"],
+        "July": ["Hiking", "Hot springs visit"],
+        "August": ["Rock climbing", "Fishing"],
+        "September": ["Mountain biking", "Northern Lights tour"],
+        "October": ["Horseback riding", "Geothermal spa"],
+        "November": ["ATV tour", "Bird watching"],
+        "December": ["Dog sledding", "Ice cave tour"],
+    }
+    current_date = datetime.datetime.now()
+
+    current_month = current_date.strftime("%B")
+
+    return schedule[current_month]
 
 
 def Reccomendations(intrests: list):
@@ -25,3 +51,7 @@ def Reccomendations(intrests: list):
 
 def Comment_On_Activity(comment: str):
     pass
+
+
+Result = Monthly_Calander()
+print(Result)
